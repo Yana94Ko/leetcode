68ab4573d47ac9f9c9e751770033b82fabdbc0fe
YanaKo <91898700+Yana94Ko@users.noreply.github.com>
2024-10-21 18:10:00 +0900

    Time: 0 ms (100%), Space: 4.1 MB (5.3%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.go
+++ 0088-merge-sorted-array/0088-merge-sorted-array.go
@@ -1,8 +1,6 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    for i:=m; i<m+n; i++ {
-        nums1[i] = nums2[i-m]
-    }
-    sort.Slice(nums1, func(j, k int) bool {
-        return nums1[j] < nums1[k]
-    })
+    for i := 0; i < n; i++ {
+		nums1[m+i] = nums2[i]
+	}
+	sort.Ints(nums1)
 }
